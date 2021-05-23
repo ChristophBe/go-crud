20dc7a4611cb4dede4544171ffa4eb431be53580
Christoph Becker <post@christophb.de>
2021-05-23 10:23:52 +0200

    test: refactor and test delete handler

--- handlers/delete.go
+++ handlers/delete.go
@@ -1,28 +1,31 @@
 package handlers
 
 import (
-	"context"
+	"github.com/ChristophBe/go-crud/types"
 	"net/http"
 )
 
-func (c crudHandlersImpl) Delete(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-	r = r.WithContext(ctx)
+// NewDeleteHandler returns a http handler for that handles the deletion of one specific model.
+func NewDeleteHandler(service types.GetOneService, responseWriter types.ResponseWriter, errorWriter types.ErrorResponseWriter) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		model, err := service.GetOne(request)
+		if err != nil {
+			errorWriter(err, writer, request)
+			return
+		}
 
-	model, err := c.service.GetOne(r)
-	if err != nil {
-		c.errorWriter(err, w, r)
-		return
-	}
+		if err = model.Delete(request.Context()); err != nil {
+			errorWriter(err, writer, request)
+			return
+		}
 
-	if err = model.Delete(ctx); err != nil {
-		c.errorWriter(err, w, r)
-		return
+		if err = responseWriter(nil, http.StatusNoContent, writer, request); err != nil {
+			errorWriter(err, writer, request)
+		}
 	}
+}
 
-	if err = c.responseWriter(nil, http.StatusNoContent, w, r); err != nil {
-		c.errorWriter(err, w, r)
-	}
+// Delete is a http handler for handling the deletion of specific model.
+func (c crudHandlersImpl) Delete(writer http.ResponseWriter, request *http.Request) {
+	NewDeleteHandler(c.service, c.responseWriter, c.errorWriter).ServeHTTP(writer, request)
 }
